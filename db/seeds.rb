a5a45cb2edf41e813a75ee7f9385bd17f9d1e629
alexbotha <alexbotha@hotmail.co.uk>
2023-02-06 15:25:09 +0000

    will add some seed data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,32 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+num_restuarants = 10 
+
+num_restuarants.times do 
+  Restuarant.create(
+    name: Faker::Name.name,
+    img: Faker::LoremFlickr.image(size: '300x200'),
+    star: rand(0..5),
+    location: Faker::Address.city,
+    average_price: rand(20..50)
+  )
+ 
+  review = Review.create(
+    name: Faker::Name.name, 
+    review: Faker::lorem.sentence
+  )
+
+  (1..num_restuarants).to_a.each do |item|
+    RestuarantReview.create(
+      restuarant_id: item,
+      review_id: review.id
+    )
+
+  end 
+end 
+
+
+
+
 puts "✅ Done seeding!"
