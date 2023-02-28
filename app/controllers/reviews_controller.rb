38ebab0be8a6f1456ac351fd857cd603f71ad47e
alexbotha <alexbotha@hotmail.co.uk>
2023-02-28 12:34:05 +0000

    will tidy up post request

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,8 +24,7 @@ post "/restuarants/:restuarant_id/reviews" do
   restuarant = Restuarant.find_by(id: params[:restuarant_id])
   review = Review.create(
     review: params[:review].capitalize(), rating: params[:rating],  
-    company_name: Restuarant.find_by(id: params[:restuarant_id]).name,
-    restuarant_id: Restuarant.find_by(id: params[:restuarant_id]).id
+    company_name: restuarant.name, restuarant_id: restuarant.id
     )
     review.to_json(include: :restuarant)      
 end 
